4e45b906a4bd858d6614637821b682842a79bcc8
Benjamin Onoja <onojapeter90@gmail.com>
2024-06-20 17:16:59 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+// /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    fontSize: {
+      sm: ['14px', '1.25rem'],
+      base: ['16px', '1.25rem'],
+      lg: ['20px', '1.25rem'],
+      xl: ['24px', '1.25rem'],
+      leadText: ['24px', '5rem'],
+    },
+    extend: {
+      colors: {
+        lightwhite: '#f7faff',
+      },
+      backgroundImage:{
+        bgabout: ''
+      }
+    },
+  },
+  plugins: [],
+}
+
+ 
