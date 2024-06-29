bec1e32e9f0948374492931207ce43fd1c879699
Benjamin Onoja <onojapeter90@gmail.com>
2024-06-29 13:58:57 +0100

    created the header and head ejs partials for the profile page

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const {dashboard, changepw, deposit, withdraw, kyc, support, investment, profile, investPlan, transactions} = require('../controllers/profileController')
 
 router.get('/dashboard', dashboard);
-router.get('/changepw', changepw);
+router.get('/change-password', changepw);
 router.get('/withdraw', withdraw);
 router.get('/kyc', kyc);
 router.get('/support', support);
 router.get('/investment', investment);
 router.get('/deposit', deposit);
 router.get('/profile', profile);
-router.get('/investPlan', investPlan);
+router.get('/investment-plans', investPlan);
 router.get('/transactions', transactions);
 
 module.exports = router;
